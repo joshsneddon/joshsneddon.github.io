399b8cd4081aab96295a5297b4be8df84ffc7fa9
Joshua Sneddon <joshuasneddon@JoshuaSddonsMBP.home>
2016-09-25 20:16:59 +0100

    Smooth page scrolling animation applied to page links

--- js/main.js
+++ js/main.js
@@ -23,6 +23,22 @@ $(function() {
 	});
 });
 
+// Smooth page scrolling
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 60
+        }, 750);
+        return false;
+      }
+    }
+  });
+});
+
 // A background color is appled to the header when the user starts scrolling
 $(function() {
     $(window).scroll(function() {
